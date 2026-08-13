322dde3ce1c3402a5aab1c0366f7d7226e2ebaa1
oopsenheimer <anujverma3563@gmail.com>
2026-08-13 09:34:26 +0530

    Added Output Files

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
 constexpr const char* MARKET_DATA_FILENAME = "market_data.csv";
 constexpr const char* USER_DATA_FILENAME = "user_data.csv";
 
+constexpr const char* H1_BARS_OUTPUT_FILENAME = "bars-1h.csv";
+constexpr const char* D1_BARS_OUTPUT_FILENAME = "bars-1d.csv";
+constexpr const char* D30_BARS_OUTPUT_FILENAME = "bars-30d.csv";
+
 int main() {
     Source market_data_file(MARKET_DATA_FILENAME);
     Source user_data_file(USER_DATA_FILENAME);
@@ -20,6 +24,8 @@ int main() {
     auto transaction = transaction_data_parser(user_data_file.read_line());
 
     Engine barf;
+    barf.initilize_output_files(
+        {H1_BARS_OUTPUT_FILENAME, D1_BARS_OUTPUT_FILENAME, D30_BARS_OUTPUT_FILENAME});
 
     while (market_tick.is_valid || transaction.is_valid) {
         Time m_ts = market_tick.is_valid ? market_tick.timestamp : std::numeric_limits<Time>::max();
